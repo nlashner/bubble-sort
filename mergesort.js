d1c09c6afa3463971194056a4a18c52e0b45c2a1
Nataliia Riabkova <nataliiariabkova@gmail.com>
2020-04-07 16:33:38 -0400

    recursion is done

--- mergesort.js
+++ mergesort.js
@@ -16,6 +16,9 @@ function merge (firstHalf, secondHalf) {
   for (let i = 0; i < firstHalf.length; i++){
     let first = firstHalf[i]
     let second = secondHalf[i]
+
+    console.log("FIRST _______>",first)
+    console.log("SECOND _______>",second)
     if(first < second){
       sortedArr.push(first)
       sortedArr.push(second)
@@ -35,22 +38,20 @@ function merge (firstHalf, secondHalf) {
 
 
 function mergeSort(array){
+debugger;
+   if ( array.length <= 1){ //------> base case
+       return array
+   } else {
+       let firstSplit = split(array)
+       let one = firstSplit[0]
+       let two = firstSplit[1]
 
-  let firstSplit = split(array)
-
-  while (firstSplit[0].length > 1){
-  let one = mergeSort(firstSplit[0])
-  let two = mergeSort(firstSplit[1])
-  }
-  merge(one, two)
-
-  let newArr = one.concat(two)
-
-  return newArr
+       return merge(mergeSort(one),mergeSort(two))
+   }
 }
 
-console.log(mergeSort([1,3]))
-
+//console.log(mergeSort([3,5,2,1]))
+console.log(merge([3,5], [1,2]))
 
 //  [1, 5, 4, 2, 6]
 // [[1, 5], [4, 2, 6]]
